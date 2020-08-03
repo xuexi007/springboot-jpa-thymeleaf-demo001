1336a43083a4984a4340954ec87b54798943b42e
myjava001 <hk109@126.com>
2020-08-03 16:48:19 +0800

    小张，修改提交

--- src/main/java/com/offcn/pay/impl/PayServiceImpl.java
+++ src/main/java/com/offcn/pay/impl/PayServiceImpl.java
@@ -7,12 +7,19 @@ import java.util.Map;
 public class PayServiceImpl implements PayService {
     @Override
     public void createNative(String out_trade_no, String price) {
-        System.out.printf("预下单操作代码");
+        //小张修改代码
+        for (int i = 0; i <100 ; i++) {
+            System.out.printf("预下单操作代码");
+        }
+
     }
 
     @Override
     public Map queryStatus(String out_trade_no) {
-        System.out.printf("查询交易状态代码");
+        //小张修改代码
+        if(out_trade_no!=null) {
+            System.out.printf("查询交易状态代码");
+        }
         return null;
     }
 }
